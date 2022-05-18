604211534fcdabf2c81c39a30c4ff118f893aeab
Shakib Hasan <86948366+shakibhasan725@users.noreply.github.com>
2022-05-18 19:14:56 +0600

    Add files via upload

---
+++ class 1/app.js
@@ -0,0 +1,8 @@
+let name = 'shakib'
+
+console.log('My name is ' + name)
+
+let dev_name = prompt('What is your name?')
+let dev_age = prompt('What is your age?')
+
+console.log(`Hi ${dev_name}, You are ${dev_age} years old. `)
